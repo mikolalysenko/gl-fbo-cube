7a8d5d2a387bb6c196a78e2a0868c1d2b099e0cb
Mikola Lysenko <mikolalysenko@gmail.com>
2014-02-02 15:45:29 -0600

    initialize repo

---
+++ README.md
@@ -0,0 +1,5 @@
+gl-fbo-cube
+===========
+Creates a frame buffer object for a cube map.
+
+**WORK IN PROGRESS**
---
+++ fbocube.js
@@ -0,0 +1,6 @@
+"use strict"
+
+module.exports = createFBOCube
+
+function createFBOCube(gl, width, height, options) {
+}
